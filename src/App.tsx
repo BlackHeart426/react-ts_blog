ed523998641b9b989e18178057d5e47b064234a7
BlackHeart <valepa007@gmail.com>
2020-04-07 13:25:13 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import { Navbar } from './components/Navbar';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import { PageBlog } from './page/pageBlog';
 import TemporaryDrawer from "./components/Drawer/Drawer";
+import {makeStyles, Theme} from '@material-ui/core';
 
 const App: React.FC = () => {
-  return <div className="grey lighten-3">
+  return <div>
     <Navbar/>
     <TemporaryDrawer open={true}/>
     <div className="Layout">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/:userId" component={PageBlog}/>
-        </Switch>
-      </BrowserRouter>
+
+        <main>
+          <div>
+            <Switch>
+              <Route exact path="/:userId" component={PageBlog}/>
+            </Switch>
+          </div>
+        </main>
     </div>
   </div>
 }
