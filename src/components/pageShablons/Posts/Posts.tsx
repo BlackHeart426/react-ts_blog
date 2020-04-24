2da3bb2d555cf65d65fe97d066d141cdc578d438
BlackHeart <valepa007@gmail.com>
2020-04-24 15:40:17 +0300

    comments

--- src/components/pageShablons/Posts/Posts.tsx
+++ src/components/pageShablons/Posts/Posts.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {Card, CardContent, Divider, Grid, IconButton, InputAdornment, Paper, Typography, Avatar} from "@material-ui/core";
+import React, {useEffect, useState, useRef} from "react";
+import {Card, CardContent, Divider, Grid, IconButton, InputAdornment, Paper, Typography, Avatar, Button} from "@material-ui/core";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
@@ -12,8 +12,9 @@ import {EditPost} from "./EditPost";
 import Skeleton from "@material-ui/lab/Skeleton";
 import {updateLikeCommentDataBlogActionCreator} from "../../../store/action/blog";
 import {useParams} from "react-router";
-import {red} from "@material-ui/core/colors";
-
+import moment from "moment";
+import shortid from "shortid";
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -38,12 +39,16 @@ interface ParamTypes {
     userId: string
 }
 
+const initialComments = {
+    comments: []
+}
+
 function Posts (props: any) {
     const {userId} = useParams<ParamTypes>();
     const {editable} = props
     const classes = useStyles()
     const [posts, setPosts] = useState([])
-    const [currentComment, setCurrenComment] = useState({uuid: '', value: ''})
+    const [currentComment, setCurrenComment] = useState<any>([])
 
     useEffect(()=>{
         if(props.dataBlog) {
@@ -81,14 +86,35 @@ function Posts (props: any) {
     }
 
     const handleChangeComment = (event: any) => {
-        setCurrenComment({...currentComment, uuid: event.currentTarget.name, value: event.currentTarget.value})
+        setCurrenComment({...currentComment, [event.target.name]:  {uuid: event.target.name, value: event.target.value}})
     }
-    const handleSendComment = (e: any) => {
-        //email, createComment
-        if(e.which === 13 ) {
-            console.log(currentComment)
+    const handleSendComment = (uuid: string) => {
+
+        const userUuid = localStorage.getItem('userId')
+        const dataComment = {
+            uuidComment: shortid.generate(),
+            userUuid,
+            text: currentComment[uuid].value,
+            createComment:  moment().format('DD MMMM  YYYY, h:mm'),
         }
 
+        const data: any = Object.values(props.dataBlog).find((item: any, index) => item.uuid === uuid)
+
+        const dataPost = {
+            uuid,
+            createPost: data.createPost,
+            name: data.name,
+            description: data.description,
+            teaser: data.teaser,
+            available: data.available,
+            comments: data.comments,
+            visible: data.visible,
+            countLike: data.countLike,
+            countComments: Object.values(data.countComments).concat(dataComment)
+        }
+        console.log(dataPost)
+        props.action.updateDataBlog(userId, 'Posts', dataPost, uuid)
+        setCurrenComment({...currentComment, [uuid]:  {uuid: uuid, value: ''}})
     }
 
     const handleChangeLike = (uuid: string) => {
@@ -106,6 +132,36 @@ function Posts (props: any) {
             countComments: data.countComments
         }
         props.action.updateDataBlog(userId, 'Posts', dataPost, uuid)
+    }
+
+    const changeComment = (data: any, uuidComment: string) => {
+        let list: any = Object.values(data.countComments).filter((item: any) => item.uuidComment !== uuidComment)
+        if(list.length === 0) {
+            list = ''
+            return list
+        } else {
+            return list
+        }
+    }
+
+    const handleDeleteComment = (uuidPost: string, uuidComment: string) => {
+        const data: any = Object.values(props.dataBlog).find((item: any, index) => item.uuid === uuidPost)
+        console.log(Object.values(data.countComments))
+        console.log(uuidComment)
+        const dataPost = {
+            uuid: uuidPost,
+            createPost: data.createPost,
+            name: data.name,
+            description: data.description,
+            teaser: data.teaser,
+            available: data.available,
+            comments: data.comments,
+            visible: data.visible,
+            countLike: data.countLike,
+            countComments: changeComment(data, uuidComment)
+        }
+        console.log(dataPost)
+        props.action.updateDataBlog(userId, 'Posts', dataPost, uuidPost)
     }
 
     return (
@@ -144,7 +200,7 @@ function Posts (props: any) {
                                 >
                                     <ChatBubbleOutlineIcon/>
                                 </IconButton>
-                                0   Comments
+                                {item.countComments && item.countComments.length}   Comments
                             </Grid>
                             <Grid item xs={2}>
                                 <IconButton
@@ -169,32 +225,59 @@ function Posts (props: any) {
                         </Grid>
                     </CardContent>
                     <Divider />
+
                     <CardContent  style={{paddingBottom: 10}}>
+                        {item.countComments && Object.values(item.countComments).map((comment: any, index: number) => (
+                            <Grid container spacing={3} key={index}>
+                                <Grid item xs={1}>
+                                    <Avatar alt="Remy Sharp" src={props.avatar}/>
+                                </Grid>
+                                <Grid item xs={10}>
+                                    <Typography variant="body2" component="p" align={"left"}>{comment.userUuid}</Typography>
+                                    <Typography variant="body2" component="p" align={"left"}>{comment.text}</Typography>
+                                    <Typography color="textSecondary" variant="body2" component="p" align={"left"}>
+                                        {comment.createComment}
+                                    </Typography>
+                                </Grid>
+                                <Grid item xs={1}>
+                                    <IconButton aria-label="delete" onClick={() => handleDeleteComment(item.uuid, comment.uuidComment)}
+                                                size={"small"}>
+                                        <DeleteIcon/>
+                                    </IconButton>
+                                </Grid>
+                            </Grid>
+                        ))
+
+                        }
                         <Grid container spacing={3}>
                             <Grid item xs={1}>
                                 <Avatar alt="Remy Sharp" src={props.avatar} />
                             </Grid>
-                            <Grid item xs={11}>
+                            <Grid item xs={9}>
                                 <TextField
                                     variant="outlined"
                                     style={{marginTop: 0,  marginBottom: 5, width: '100%'}}
                                     id="comment"
                                     name={item.uuid}
                                     type="comment"
+                                    value={currentComment[item.uuid] ? currentComment[item.uuid].value : ''}
                                     size={"small"}
                                     onChange={handleChangeComment}
-                                    onKeyPress={(e) => handleSendComment(e)}
-                                    InputProps={{
-                                        endAdornment: <TelegramIcon  fontSize={"large"}/>,
-                                    }}
-                                    // label="Comment"
                                     placeholder="Write comment"
                                     margin="normal"
                                 />
                             </Grid>
+                            <Grid item xs={2}>
+                                <Button
+                                    disableElevation
+                                    color={"primary"}
+                                    variant={"contained"}
+                                    aria-label="delete"
+                                    onClick={() => handleSendComment(item.uuid)}>
+                                    <TelegramIcon/>
+                                </Button>
+                            </Grid>
                         </Grid>
-
-
                     </CardContent>
                 </Paper>
                 ))
