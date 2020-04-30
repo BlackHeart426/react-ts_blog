4de0eca7078ea481faa61d49261e52ecea1ae72c
BlackHeart <valepa007@gmail.com>
2020-04-30 15:42:14 +0300

    type reduce

--- src/store/reducers/app.tsx
+++ src/store/reducers/app.tsx
@@ -1,16 +1,21 @@
 import {OPEN_DRAWER} from "../types";
+import * as actions from '../action/app'
+
+type InferValueTypes<T> = T extends { [key: string]: infer U} ? U : never;
+type ActionTypes = ReturnType<InferValueTypes<typeof actions>>
 
 export const initialState = {
-    alert: null,
+    alert: false,
     openDrawer: false
 }
 
-interface IAction {
-    type: string,
-    payload: () => {alert: boolean, openDrawer: boolean}
+interface StateType {
+    openDrawer: boolean,
+    alert: boolean
 }
 
-export const appReducer = (state: object = initialState, action: IAction) => {
+
+export const appReducer = (state: StateType = initialState, action: ActionTypes) => {
     switch (action.type) {
         case OPEN_DRAWER:
             return {...state, openDrawer: action.payload }
